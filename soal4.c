74f510411b6c15e8e705f922cb90fe9fe374d9fb
yasintar <32873398+yasintar@users.noreply.github.com>
2017-10-24 17:54:56 +0100

    Add files via upload

---
+++ soal4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+pthread_t tid[100];
+int n;
+
+void* faktorial(){
+
+	int bilangan=1;
+	for(int a=1; a<=n; a++){
+		bilangan = bilangan * a;
+	}
+}
+
+int main(void){
+
+	
+
+	
+	
+	return 0;
+	
+}
