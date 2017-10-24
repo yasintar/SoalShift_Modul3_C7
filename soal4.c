32af7b0a4a3fd6d0da8037740edd10ccb82ac00f
yasintar <32873398+yasintar@users.noreply.github.com>
2017-10-24 18:46:17 +0100

    Update soal4.c

--- soal4.c
+++ soal4.c
@@ -8,8 +8,9 @@
 pthread_t tid[100];
 int n;
 
-void* faktorial(){
+void* faktorial(void *arg){
 
+//ingatyasdisinikurang 	
 	int bilangan=1;
 	for(int a=1; a<=n; a++){
 		bilangan = bilangan * a;
@@ -18,7 +19,26 @@ void* faktorial(){
 
 int main(void){
 	
+	int i=0;
+//	int err;
+	int input;
 	
+	scanf("%d",&input);
+	
+/*	while(i<n){
+		err = pthread_create(&(tid[i]),NULL,&faktorial,NULL);
+		if(err != 0){
+			printf("\nCan't create thread : [%s]",strrerror(err));
+		} else {
+			printf("\nCreate thread %d success\n",i);
+		}
+		i++;
+	} */
+	
+	pthread_create(&(tid[i]),NULL,&faktorial);	
+	
+	pthread_join(tid,NULL);
+			
 
 	
 	
