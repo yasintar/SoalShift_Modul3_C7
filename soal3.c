a0a28156f2bef21dbca9a79ec14f0f3e20cc6b85
ahmadkikok <ahmadshidqifirdaus@gmail.com>
2017-10-25 02:07:38 +0700

    Finishing soal3.c

--- soal3.c
+++ soal3.c
@@ -5,29 +5,76 @@
 #include <string.h>
 
 pthread_t tid[2];
+int mkn, stat[2]={100,100}; // 0 = Lohan, 1 = Kepiting
+int loop=1;
 
 void* playandcount(void *arg) {
     pthread_t id=pthread_self();
-    int mkn, stat[2]={100,100}; // 0 = Lohan, 1 = Kepiting
     if(pthread_equal(id,tid[0])){//thread untuk menjalankan counter
     while(1){
-	printf("Lohan %d\n", stat[0]);
+	system("clear");
 	printf("Kepiting %d\n", stat[1]);
+	printf("Lohan %d\n", stat[0]);
 	printf("1. Beri Makan Kepiting\n");
 	printf("2. Beri Makan Lohan\n");
 	scanf("%d", &mkn);
 	if(mkn==1)stat[1]+=10;
 	if(mkn==2)stat[0]+=10;
-	//if(stat[0]>100||stat[0]<0||stat[1]>100||stat[1]<0)break;
+	if(stat[0]>100||stat[0]<0||stat[1]>100||stat[1]<0){
+		if(stat[0]>stat[1]){
+			system("clear");
+			printf("Lohan Win\n");			
+			exit(0);
+		}
+		if(stat[1]>stat[0]){
+			system("clear");
+			printf("Kepiting Win\n");	
+			exit(0);		
+		}
 	}
     }
+}
     else if(pthread_equal(id,tid[1])){
     while(1){
 	sleep(10);
 	stat[0]-=15;
+	if(stat[0]>100||stat[0]<0||stat[1]>100||stat[1]<0){
+		if(stat[0]>stat[1]){
+			system("clear");
+			printf("Lohan Win\n");			
+			exit(0);
+		}
+		if(stat[1]>stat[0]){
+			system("clear");
+			printf("Kepiting Win\n");	
+			exit(0);		
+		}
+	}
+	system("clear");
+	printf("Kepiting %d\n", stat[1]);
+	printf("Lohan %d\n", stat[0]);
+	printf("1. Beri Makan Kepiting\n");
+	printf("2. Beri Makan Lohan\n");
 	sleep(10);
 	stat[1]-=10;
-	stat[0]-=15;	
+	stat[0]-=15;
+	if(stat[0]>100||stat[0]<0||stat[1]>100||stat[1]<0){
+		if(stat[0]>stat[1]){
+			system("clear");
+			printf("Lohan Win\n");			
+			exit(0);
+		}
+		if(stat[1]>stat[0]){
+			system("clear");
+			printf("Kepiting Win\n");	
+			exit(0);		
+		}
+	}
+	system("clear");
+	printf("Kepiting %d\n", stat[1]);
+	printf("Lohan %d\n", stat[0]);
+	printf("1. Beri Makan Kepiting\n");
+	printf("2. Beri Makan Lohan\n");
 	}
     }
     return NULL;
@@ -40,14 +87,6 @@ int main(void)
     while(i<2)//looping membuat thread 2x
     {
         err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);//membuat thread
-        if(err!=0)//cek error
-        {
-            printf("\n can't create thread : [%s]",strerror(err));
-        }
-        else
-        {
-            printf("\n create thread success");
-        }
         i++;
     }
     pthread_join(tid[0],NULL);
