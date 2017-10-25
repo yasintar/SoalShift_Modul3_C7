1d7b778038afda9bd5d8d3994c075b644b14dadb
ahmadkikok <ahmadshidqifirdaus@gmail.com>
2017-10-25 16:01:22 +0700

    Fix segment fault if missing novel.txt and remove error checking

--- soal5.c
+++ soal5.c
@@ -9,9 +9,13 @@ void* findstring(void *arg){
 	char *string=(char*)arg;
 	FILE *fp=fopen("novel.txt","r");
 	char len[256];
+	if(fp==NULL){
+	printf("File not found!\n");
+	} else {
 	while(fscanf(fp,"%s",len)!=EOF)
 	if(!strncmp(len,string,strlen(string)))count++;
 	printf("%s : %d \n", arg, count);
+	}
 }
 
 int main(int argc, char *argv[]){
@@ -24,11 +28,6 @@ int main(int argc, char *argv[]){
 		printf("Masukan minimal 2 argument\n");
 	}
 
-	for(i=1;i<argc;i++){
-		int err;
-		err = pthread_create(&(tid[i]),NULL,&findstring,(void*) argv[i]);
-		if(err!=0)printf("Gagal create thread\n");
-		else printf("Berhasil membuat thread\n");
-	}
+	for(i=1;i<argc;i++)pthread_create(&(tid[i]),NULL,&findstring,(void*) argv[i]);
 	for(i=1;i<argc;i++)pthread_join(tid[i],NULL);
 }
