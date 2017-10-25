4cb8ddc229cf47df55e89fff3c79cac7fac33487
ahmadkikok <ahmadshidqifirdaus@gmail.com>
2017-10-25 12:09:05 +0700

    Finishing code and remove checking error

--- soal4.c
+++ soal4.c
@@ -6,13 +6,13 @@
 #include<stdlib.h>
 
 pthread_t tid;
-int n;
 
 void* faktorial(void *arg){
 	int i;
+	int asu = (int)arg;
 	unsigned long long bilangan=1;
-	for(i=1; i<=n; i++)bilangan = bilangan * i;
-	printf("Hasil %d! = %llu\n",n,bilangan);
+	for(i=1; i<=asu; i++)bilangan = bilangan * i;
+	printf("Hasil %d! = %llu\n",arg,bilangan);
 }
 
 int main(int argc, char *argv[]){
@@ -25,20 +25,16 @@ int main(int argc, char *argv[]){
 		printf("Masukan minimal 2 argument\n");
 	}
 
-	for(i=1;i<argc;i++){
+	for(i=1;i<argc;i++){		
      		char* s = argv[i];
      		char* num_ptr = s;
      		while(*num_ptr < '0' || *num_ptr > '9')++num_ptr;
 		int number = atoi(num_ptr);
-	 	printf("%d\n", number);
-		//printf("%d", jum);
-	}
-
-	for(i=1;i<argc;i++){
-		int err;
-		err = pthread_create(&(tid[i]),NULL,&faktorial,(void*)number);
-		if(err!=0)printf("Gagal create thread\n");
-		else printf("Berhasil membuat thread\n");
+		pthread_create(&(tid[i]),NULL,&faktorial,(void*)number);
+		//err = pthread_create(&(tid[i]),NULL,&faktorial,(void*)number);
+		//int err;		
+		//if(err!=0)printf("Gagal create thread\n");
+		//else printf("Berhasil membuat thread\n");
 	}
 	for(i=1;i<argc;i++)pthread_join(tid[i],NULL);
 }
